bcaf97fbf14a69a3e196b5eaa848404f618aa73b
dennis101251 <dengzexi2008@gmail.com>
2017-02-06 23:49:27 +0100

    basic function

--- iConnect/AppDelegate.swift
+++ iConnect/AppDelegate.swift
@@ -11,16 +11,38 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "Bone")
+            button.action = #selector(AppDelegate.printQuote(sender: ))
+        }
+        
+        let menu = NSMenu()
+        
+        menu.addItem(NSMenuItem(title: "Print Quote", action: #selector(AppDelegate.printQuote(sender: )), keyEquivalent: "P"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit Quotes", action: #selector(AppDelegate.quit(sender: )), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func printQuote(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        print("\(quoteText) — \(quoteAuthor)")
+    }
+    
+    func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
 }
 
