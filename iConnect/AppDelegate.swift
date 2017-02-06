f286b5b1d907722ff4b0e43fadf3e1451ff5ca63
dennis101251 <dengzexi2008@gmail.com>
2017-02-06 22:16:57 +0100

    Initial Commit

---
+++ iConnect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iConnect
+//
+//  Created by 邓泽西 on 2017/2/6.
+//  Copyright © 2017年 ecp. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iConnect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iConnect
+//
+//  Created by 邓泽西 on 2017/2/6.
+//  Copyright © 2017年 ecp. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
